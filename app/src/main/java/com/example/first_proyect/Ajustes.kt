c94b550d2d8cb4f99d2d690980e43bfa51331365
R0s4l3s <c4rl0sra@gmail.com>
2024-06-02 16:15:29 -0500

    cambio

--- app/src/main/java/com/example/first_proyect/Ajustes.kt
+++ app/src/main/java/com/example/first_proyect/Ajustes.kt
@@ -1,27 +1,20 @@
 package com.example.first_proyect
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Pantalla5 : AppCompatActivity() {
+class Ajustes : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_pantalla5)
+        setContentView(R.layout.activity_ajustes)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val btnr: Button = findViewById(R.id.siguiente)
-        btnr.setOnClickListener{
-            val intent: Intent = Intent(this, Sistema_P::class.java)
-            startActivity(intent)
         }
     }
 }
