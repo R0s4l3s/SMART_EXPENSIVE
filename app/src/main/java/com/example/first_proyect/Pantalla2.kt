773fed43bbc159ee52b28b6967142350c97ba432
R0s4l3s <c4rl0sra@gmail.com>
2024-06-02 00:09:43 -0500

    avance

--- app/src/main/java/com/example/first_proyect/Pantalla2.kt
+++ app/src/main/java/com/example/first_proyect/Pantalla2.kt
@@ -8,19 +8,24 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Pantalla2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pantalla2)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btn: Button = findViewById(R.id.IngresarP1)
-        btn.setOnClickListener{
-            val intent: Intent = Intent(this, Pantalla2::class.java)
+        val btnr: Button = findViewById(R.id.registerButton)
+        btnr.setOnClickListener{
+            val intent: Intent = Intent(this, Pantalla3::class.java)
+            startActivity(intent)
+        }
+        val btna: Button = findViewById(R.id.accederButton)
+        btna.setOnClickListener{
+            val intent: Intent = Intent(this, Sistema_P::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/first_proyect/Pantalla3.kt
+++ app/src/main/java/com/example/first_proyect/Pantalla3.kt
@@ -8,19 +8,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Pantalla3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_pantalla3)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btn: Button = findViewById(R.id.IngresarP1)
-        btn.setOnClickListener{
-            val intent: Intent = Intent(this, Pantalla2::class.java)
+        val btns: Button = findViewById(R.id.siguiente)
+        btns.setOnClickListener {
+            val intent: Intent = Intent(this, Pantalla4::class.java)
             startActivity(intent)
         }
     }
