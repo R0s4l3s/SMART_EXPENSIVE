ada38be18407b03686718408971f8cb9228056f3
R0s4l3s <c4rl0sra@gmail.com>
2024-06-04 20:52:52 -0500

    otra actualizacion

--- app/src/main/java/com/example/first_proyect/Eusuario.kt
+++ app/src/main/java/com/example/first_proyect/Eusuario.kt
@@ -1,6 +1,8 @@
 package com.example.first_proyect
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class Eusuario : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val btnA: ImageButton = findViewById(R.id.bottonA)
+        btnA.setOnClickListener{
+            val intent: Intent = Intent(this, Ajustes::class.java)
+            startActivity(intent)
         }
     }
 }
